0c8c34948bf7ee9f24f6ed246bd8652a23bb3d3c
GeuPop <geuseportega@gmail.com>
2026-03-11 00:52:57 -0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,45 @@
+function goHome(){
+
+document.getElementById("home").classList.remove("hidden");
+document.getElementById("dudas").classList.add("hidden");
+document.getElementById("links").classList.add("hidden");
+
+}
+
+function showDudas(){
+
+document.getElementById("home").classList.add("hidden");
+document.getElementById("dudas").classList.remove("hidden");
+document.getElementById("links").classList.add("hidden");
+
+}
+
+function showLinks(){
+
+document.getElementById("home").classList.add("hidden");
+document.getElementById("dudas").classList.add("hidden");
+document.getElementById("links").classList.remove("hidden");
+
+}
+
+function goNotas(){
+
+window.open("https://geupop.github.io/HidrologiaUdeA/","_blank");
+
+}
+
+function toggle(id){
+
+var element=document.getElementById(id);
+
+if(element.classList.contains("hidden")){
+
+element.classList.remove("hidden");
+
+}else{
+
+element.classList.add("hidden");
+
+}
+
+}
